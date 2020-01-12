bf20cc7416a46b3d9061ae2baccf40c59a0f7baf
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2020-01-12 08:38:09 -0500

    Added more data options

--- src/selection.js
+++ src/selection.js
@@ -11,8 +11,14 @@ class Selection extends React.Component {
 render(props) {
     return (
         <div className={SS.select}>
-        <button onClick={this.changeSelect} className='top'>Top</button>
-        <button onClick={(e) => this.props.changeSelect(e)} className='new'>Top</button>
+        
+        <button onClick={(e) => this.props.changeSelect(e)} className='top'>Top</button>
+        <button onClick={(e) => this.props.changeSelect(e)} className='new'>New</button>
+        <button onClick={(e) => this.props.changeSelect(e)} className='hot'>Hot</button>
+        <button onClick={(e) => this.props.changeSelect(e)} className='controversial'>Controversial</button>
+        <button onClick={(e) => this.props.changeSelect(e)} className='rising'>Rising</button>
+        <button onClick={(e) => this.props.changeSelect(e)} className='best'>Best</button>
+        <button onClick={(e) => this.props.changeSelect(e)} className='comments'>Comments</button>
         <button>Top</button>
         <button>Top</button>
         <button>Top</button>
