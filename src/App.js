e382043212688b0e888c9fdc651f2ee31b7fadfb
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2020-01-12 07:09:38 -0500

    Working on adding sort by new or top buttons

--- src/App.js
+++ src/App.js
@@ -10,15 +10,23 @@ class App extends React.Component {
 
     this.state = {
       loading: true,
-      tops: false
+      tops: false,
+      selection: 'top'
     }
     this.handleClick = this.handleClick.bind(this);
+    this.changeSelect = this.changeSelect.bind(this);
+  }
+
+
+  handleSelect(e) {
+    const name = e.target.name;
+    console.log(name)
   }
 
    handleClick(e) {
      this.setState({loading: true})
     
-    const url = (`https://www.reddit.com/r${e.target.className}/top/.json?count=20`);
+    const url = (`https://www.reddit.com/r${e.target.className}/${this.state.selection}/.json?count=20`);
     fetch(url)
     .then(resp => resp.json())
     .then((data) => {
@@ -54,6 +62,12 @@ class App extends React.Component {
         <button onClick={this.handleClick} className={'/programming'}>/programming</button>
         <button onClick={this.handleClick} className={'/javascript'}>/javascript</button>
         <button>Name</button>
+      </div>
+      <div className={SS.select}>
+        <button onClick={this.changeSelect}>Top</button>
+        <button>Top</button>
+        <button>Top</button>
+        <button>Top</button>
       </div>
 
      {this.state.loading ? <div className={SS.load}><img src={logo} alt="loading"></img></div> : < Data tops={this.state.tops} /> }
